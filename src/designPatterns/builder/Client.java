c304fc630df2919fe1947fb3d0a1c905ff462e85
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2023-09-06 17:42:06 -0400

    Builder pattern structure

---
+++ src/designPatterns/builder/Client.java
@@ -0,0 +1,9 @@
+package designPatterns.builder;
+
+public class Client{
+    public static void main(String[] args) {
+        //first call builder class it validates all set params
+        //then the builder class calls the student constructor and it creates the obj
+        Student s = new builder().
+    }
+}
