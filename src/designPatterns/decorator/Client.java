0cc158213ac432da626bcf6f0c3ad85716c0a65b
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2023-09-10 19:13:55 -0400

    Observer Pattern Comments added

--- src/designPatterns/decorator/Client.java
+++ src/designPatterns/decorator/Client.java
@@ -2,6 +2,10 @@ package designPatterns.decorator;
 
 public class Client{
     public static void main(String[] args) {
+        //observer pattern is used to create objects in a sequence
+        //the object created will be of a interface type
+        //Each time an object is created its passed to its next class and is stored as private attribute to create a chain
+        //the object creation is very simple and te user just needs to call the constructors in a sequence
         Icecream icecream = new ChocolateChip(
                                 new ChocolateScoop(
                                     new VanillaScoop(
