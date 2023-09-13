448471a39b2b99881fc7862a97cfae5100e532fa
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2023-09-13 17:07:30 -0400

    Strategy Design Pattern

---
+++ src/designPatterns/strategy/BikePathCalculatorStrategy.java
@@ -0,0 +1,27 @@
+package designPatterns.strategy;
+
+public class BikePathCalculatorStrategy implements PathCalculatorStrategy {
+
+    private static BikePathCalculatorStrategy instance;
+
+    private BikePathCalculatorStrategy(){
+
+    }
+
+    public static BikePathCalculatorStrategy getInstance() {
+        if(instance == null) {
+            synchronized (BikePathCalculatorStrategy.class) {
+                if (instance == null) {
+                    instance = new BikePathCalculatorStrategy();
+                }
+            }
+        }
+        return instance;
+    }
+
+
+    @Override
+    public void findPath(String source, String destination) {
+        System.out.println("Path calculation via Bike from : " + source + " to " + destination );
+    }
+}
